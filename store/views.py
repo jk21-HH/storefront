19727d8976b835e26bb396db01fd2aa01088d6cf
jk21-HH <jeanya12@gmail.com>
2026-07-18 20:03:05 +0300

    Add cart retrieval endpoint with nested items and tax-inclusive totals
    Recreate Cart/CartItem tables with UUID primary key, add CartSerializer
    with nested CartItemSerializer showing per-item and cart total price
    (quantized to 2 decimals), and wire up CartViewSet retrieve/create routes

--- store/views.py
+++ store/views.py
@@ -6,12 +6,13 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import status
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from .filters import ProductFilter
-from .models import Collection, OrderItem, Product, Review
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from .models import Cart, Collection, OrderItem, Product, Review
+from .serializers import CartSerializer, CollectionSerializer, ProductSerializer, ReviewSerializer
 from .pagination import DefaultPagination
     
 
@@ -61,3 +62,9 @@ class ReviewViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
+    
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+    queryset = Cart.objects.prefetch_related('items__product').all()
+    serializer_class = CartSerializer
+
+
